806e65da8f3c0928b0808290adcdbb3fe4da6416
Felipe <feli.vega@duocuc.cl>
2024-09-09 10:35:39 -0300

    Barra de Navegación, Links configurados

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,11 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// COMPONENTES
+import Header from "@/components/header";
+
+
+//CONFIGURACIÓN DE FUENTE
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"], 
   weight: ["100","200", "300","400", "500","600", "700", "800"],
@@ -15,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+      <Header />
+      {children}</body>
+     
     </html>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// COMPONENTES
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* Logo */}
+            <Link href="/">
+                <h1 className="text-4xl font-semibold"> 
+                    Felipe Vega <span className="text-accent">.</span>
+                </h1>
+            </Link>
+
+
+
+            {/* Barra de navegación Escritorio & Botón de contratación */}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav />
+                <Link href="/contacto">
+                    <Button>Contratame</Button>
+                </Link>
+            </div>
+
+            {/* Barra de navegación Movil */}
+
+            <div className="xl:hidden">Movil</div>
+            
+        </div>
+    </header>
+  );
+};
+
+export default Header;
